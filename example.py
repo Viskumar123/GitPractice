32cf2ae764d67f240564b441b561b321e1ac9377
Viskumar123 <67327149+Viskumar123@users.noreply.github.com>
2020-10-01 20:38:43 +0530

    yo yo

--- example.py
+++ example.py
@@ -2,4 +2,5 @@ def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
 # for print somthing
-print("hello world")   
+print("hello world")
+print("Yo yo")
